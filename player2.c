ef83b1753cd5b045445735398defa5ed4dd55a53
Adam Abbas <aabbas@stuy.edu>
2018-01-18 15:33:13 -0500

    Working on two way connection. Progress!

--- player2.c
+++ player2.c
@@ -5,10 +5,9 @@ int main() {
   int to_server;
   int from_server;
   int from_serv;
-  int to_serv
+  int to_serv;
 
   from_server = client_handshake(&to_server);
-  to_serv = server_handshake(&from_serv);
 
   printf("[player2] connected to player 1!\n");
 
@@ -19,6 +18,17 @@ int main() {
     *strchr(name, '\n') = 0;
 
     write(to_server, name, sizeof(char *));
+
+    printf("[player2] write complete \n");
+    printf("[player2] beginning handshake... \n");
+    to_serv = server_handshake_two(&from_serv);
+    printf("[player2] handshake complete... \n");
+
+    char p1name[100];
+    printf("preparing to read from player1!! %d \n", to_serv);
+    read(to_serv, p1name, sizeof(char*));
+
+    printf("Hi %s! Looking forward to playing with u. \n", p1name);
 
     char tic_tac_toe[3][3];
 
--- player_connection.c
+++ player_connection.c
@@ -9,16 +9,12 @@ int server_handshake(int *to_client){
   read(from_client, buffer, sizeof(buffer));
   printf("[player1] received [%s]\n", buffer);
   remove("mario");
-  printf("[player1] removed well known connection");
+  printf("[player1] removed well known connection\n");
 
   *to_client = open(buffer, O_WRONLY, 0);
   write(*to_client, buffer, sizeof(buffer));
 
-  read(from_client, buffer, sizeof(buffer));
-  printf("[player1] received: %s\n", buffer);
-
   return from_client;
-
 }
 
 int client_handshake(int *to_server){
@@ -38,6 +34,44 @@ int client_handshake(int *to_server){
 
   remove(buffer);
   printf("[player2] removed private pipe\n");
+
+  return from_server;
+}
+
+int server_handshake_two(int *to_client){
+  int from_client;
+  char buffer[10000];
+  mkfifo("game", 0600);
+  printf("[player2] Waiting for player 1... \n");
+  from_client = open("game", O_RDONLY, 0);
+  read(from_client, buffer, sizeof(buffer));
+  printf("[player2] received [%s]\n", buffer);
+  remove("mario");
+  printf("[player2] removed well known connection\n");
+
+  *to_client = open(buffer, O_WRONLY, 0);
+  write(*to_client, buffer, sizeof(buffer));
+
+  return from_client;
+}
+
+int client_handshake_two(int *to_server){
+  int from_server;
+  char buffer[1000];
+
+  printf("[player1] connected to player2\n");
+  *to_server = open("game", O_WRONLY, 0);
+
+  sprintf(buffer, "%d", getpid());
+  mkfifo(buffer, 0600);
+
+  write(*to_server, buffer, sizeof(buffer));
+
+  from_server = open(buffer, O_RDONLY, 0);
+  read(from_server, buffer, sizeof(buffer));
+
+  remove(buffer);
+  printf("[player1] removed private pipe\n");
 
   return from_server;
 }
