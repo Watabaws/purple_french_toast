aa70f33639ac4c48e8d55b9f41efcc651a1edc2c
Cynthia Cheng <cynthiacheng1022@gmail.com>
2018-01-26 02:44:18 -0500

    fh

--- Blackjack.c
+++ Blackjack.c
@@ -2,17 +2,9 @@
 #include <time.h>
 
 
-void swap(int a[52], int b, int c){
-  int temp = a[b];
-  a[b] = a[c];
-  a[b] = temp;
-}
 
 int shuffle(int cards[52]){
   srand(time(NULL));
-  for (int i =0; i < 52; i++){
-    printf ("%d \n", cards[i]);
-  }
   for (int i = 1; i< 14; i++){
     cards[i*4-1] = i;
     cards[i*4-2] = i;
@@ -20,10 +12,13 @@ int shuffle(int cards[52]){
     cards[i*4-4] = i;
   }
   int randnum;
-  for (int i =0; i < 52; i++){
+  for (int i = 0; i < 52; i++){
     randnum = rand()%((51-0)+0);
-    printf ("%d \n", cards[i]);
-    swap(&cards[52], i, randnum);
+    int temp = cards[i];
+    cards[i] = cards[randnum];
+    cards[randnum] = temp;
+  }
+  for (int i = 0; i < 52; i++){
     printf ("%d \n", cards[i]);
   }
 }
@@ -33,15 +28,20 @@ int ConvertCardToVal(int card){
   if (card == 11 || card == 12 || card == 13){
     return 10;
   }
+  //if user chooses Ace to be 11 change its num to 14
   else if (card == 1){
-    //can be 1 or 11 tho
     return 1;
+  }
+  else if (card == 14){
+    return 11;
   }
   return card;
 }
 
 int dealCard(int i, int cards[]){
-  i = i +1;
+  if (cards[i] == 1){
+    printf("Would you like to use the Ace with a value of 1 or 11?");
+  }
   return cards[i];
 }
 
@@ -90,16 +90,17 @@ void pic(int num){
 }
 
 
-
-
 int main(){
   int deck[52] = {0};
-  shuffle(&deck[52]);
+  shuffle(deck);
   int playerOneSum = 0;
   int playerTwoSum = 0;
   int cardIn = 0;
-  // pic(dealCard(deck, cardIn));
-  // pic(dealCard(deck, cardIn));
-
+  pic(dealCard(cardIn, deck));
+  playerOneSum += ConvertCardToVal(dealCard(cardIn, deck));
+  cardIn++;
+  pic(dealCard(cardIn, deck));
+  playerOneSum += ConvertCardToVal(dealCard(cardIn, deck));
+  printf("%d \n", playerOneSum);
 
 }
